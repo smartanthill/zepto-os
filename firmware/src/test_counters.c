a1d76924f635e6aefa7b257432e3c96e321dd0f1
divanchykhin <di@o-log-n.com>
2015-07-31 14:48:13 +0300

    Further organizing code for three projects (firmware, comm-stack-server, and test(client)): deleting obsolete files and editing #include paths

--- firmware/src/test_counters.c
+++ firmware/src/test_counters.c
@@ -16,7 +16,7 @@ Copyright (C) 2015 OLogN Technologies AG
 *******************************************************************************/
 
 
-#include "common/sa_common.h"
+#include <simpleiot/siot_common.h>
 
 #ifdef ENABLE_COUNTER_SYSTEM
 size_t COUNTERS[MAX_COUNTERS_CNT];
