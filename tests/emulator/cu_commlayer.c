4c15b3d258b55ecb875cf8369d0b9884e424ca7e
divanchykhin <di@o-log-n.com>
2015-07-31 15:02:28 +0300

    Further organizing code for three projects (firmware, comm-stack-server, and test(client)): editing #include paths and cleanup

--- tests/emulator/cu_commlayer.c
+++ tests/emulator/cu_commlayer.c
@@ -16,7 +16,7 @@ Copyright (C) 2015 OLogN Technologies AG
 *******************************************************************************/
 
 #include "cu_commlayer.h"
-#include "../../firmware/src/hal/hal_common/hal_waiting.h"
+#include <hal_waiting.h>
 #include <stdio.h>
 
 #define MAX_PACKET_SIZE 50
