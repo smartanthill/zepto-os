96df84265b8c6c3ff562ba1d61eea910e925bbed
divanchykhin <di@o-log-n.com>
2015-07-31 14:58:32 +0300

    Further organizing code for three projects (firmware, comm-stack-server, and test(client)): moving and renaming files and editing #include paths

--- firmware/src/common/sa_eeprom.c
+++ firmware/src/common/sa_eeprom.c
@@ -15,7 +15,7 @@ Copyright (C) 2015 OLogN Technologies AG
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 *******************************************************************************/
 
-#include "sa_eeprom.h"
+#include <siot_eeprom.h>
 #include <hal_eeprom.h>
 
 // slot structure:
