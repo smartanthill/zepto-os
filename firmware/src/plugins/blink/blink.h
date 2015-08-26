a96d993f2a1f9b5aae094cba8af2df4e3d0f231d
Ivan Kravets <me@ikravets.com>
2015-08-26 19:56:45 +0300

    Extern C for plugins (sa_bodyparts_list.cpp)

--- firmware/src/plugins/blink/blink.h
+++ firmware/src/plugins/blink/blink.h
@@ -38,8 +38,16 @@ typedef struct _blink_plugin_persistent_state
     uint8_t dummy_byte;
 } blink_plugin_persistent_state;
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 uint8_t blink_plugin_handler_init( const void* plugin_config, void* plugin_state );
 uint8_t blink_plugin_exec_init( const void* plugin_config, void* plugin_state );
 uint8_t blink_plugin_handler( const void* plugin_config, void* plugin_persistent_state, void* plugin_state, parser_obj* command, MEMORY_HANDLE reply, waiting_for* wf, uint8_t first_byte );
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif // __SA_BLINK_PLUGIN_H__
