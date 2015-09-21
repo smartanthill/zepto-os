afad96f59bf47f9b4f7e627fae2107006ea30f83
divanchykhin <di@o-log-n.com>
2015-09-21 21:25:03 +0300

    toward simulation of multiple (Slave) Devices (loading Device ID and key, etc)

--- firmware/src/platforms/desktop/hal_main.c
+++ firmware/src/platforms/desktop/hal_main.c
@@ -18,8 +18,96 @@ Copyright (C) 2015 OLogN Technologies AG
 #include "hal_main.h"
 #include "../../sa_main.h"
 
+#ifdef SA_DYN_PAIRING_ENABLED 
+#include <stdio.h>
+bool load_device_ids()
+{
+	extern uint8_t* AES_ENCRYPTION_KEY;
+	extern uint16_t DEVICE_SELF_ID;
+
+	FILE* fme = fopen( "devid.dat", "rb" );
+	if ( fme == 0 )
+	{
+		ZEPTO_DEBUG_PRINTF_1( "failed to access \'devid.dat\' file. Terminating...\n" );
+		return false;
+	}
+	int8_t buff[ 80 ];
+	uint8_t ret = fread( buff, 1, 80, fme );
+	fclose( fme );
+	if ( ret < 50 )
+	{
+		ZEPTO_DEBUG_PRINTF_1( "file \'devid.dat\' is incomplete or corrupted. Terminating...\n" );
+		return false;
+	}
+
+	uint8_t i = 0, pos;
+	bool in_progress = false;
+
+	i = 0;
+	for ( pos=0; pos<ret; pos++ )
+	{
+		bool val_updated = true;
+		int8_t ch = buff[pos];
+		uint8_t val;
+		if ( ch >='0' && ch <='9' ) val = ch - '0';
+		else if ( ch >='a' && ch <='f' ) val = ch - 'a' + 10;
+		else if ( ch >='A' && ch <='F' ) val = ch - 'A' + 10;
+		else
+			val_updated = false;
+
+		if ( val_updated )
+		{
+			if ( in_progress )
+			{
+				if ( i < 16 )
+					AES_ENCRYPTION_KEY[i] = (AES_ENCRYPTION_KEY[i] << 4 ) | val;
+				else
+					DEVICE_SELF_ID = ( DEVICE_SELF_ID << 4 ) | val;
+				i++;
+				in_progress = false;
+			}
+			else
+			{
+				if ( i < 16 )
+					AES_ENCRYPTION_KEY[i] = val;
+				else
+					DEVICE_SELF_ID = val;
+				in_progress = true;
+			}
+		}
+		else
+		{
+			if ( in_progress )
+			{
+				i++;
+				in_progress = false;
+			}
+		}
+	}
+
+	if ( i < 17 )
+	{
+		ZEPTO_DEBUG_PRINTF_1( "file \'devid.dat\' has invalid or incomplete data, or corrupted. Terminating...\n" );
+		return false;
+	}
+
+	ZEPTO_DEBUG_PRINTF_2( "Running as: node = %d; key = ", DEVICE_SELF_ID );
+	for ( i=0; i<16; i++ )
+		ZEPTO_DEBUG_PRINTF_2( "%02x ", AES_ENCRYPTION_KEY[i] );
+	ZEPTO_DEBUG_PRINTF_1( "\n\n" );
+
+	return true;
+}
+#endif
+
+
 int main(int argc, char *argv[])
 {
+#ifdef SA_DYN_PAIRING_ENABLED 
+	if ( !load_device_ids() )
+		return 0;
+#endif // SA_DYN_PAIRING_ENABLED
+
     sa_main_init();
 
     return sa_main_loop();
--- firmware/src/zepto_config.h
+++ firmware/src/zepto_config.h
@@ -24,8 +24,23 @@ THIS FILE IS MANUALLY OR AUTOMATICALLY GENERATED BASED ON DESIRED PLUGIN LIST
 #if !defined __ZEPTO_CONFIG_H__
 #define __ZEPTO_CONFIG_H__
 
+// NOTE: this approach is subject to change; basically there are two ways how key may become available: as a result of pairing or during programming
+// in the first caes we just prepare a location where the key will be stored;
+// in the second case the key is a part of code
+// THIS APPROACH IS SUBJECT TO CHANGE
+#ifdef SA_DYN_PAIRING_ENABLED 
+
 #define DECLARE_AES_ENCRYPTION_KEY \
-const uint8_t AES_ENCRYPTION_KEY[16] ZEPTO_PROG_CONSTANT_LOCATION = \
+uint8_t AES_ENCRYPTION_KEY_STORAGE[16]; \
+const uint8_t* AES_ENCRYPTION_KEY = AES_ENCRYPTION_KEY_STORAGE;
+
+#define DECLARE_DEVICE_ID \
+uint16_t DEVICE_SELF_ID = 0xFFFF; /* definitely, not 0; must be loaded later */
+
+#else // SA_DYN_PAIRING_ENABLED
+
+#define DECLARE_AES_ENCRYPTION_KEY \
+const uint8_t AES_ENCRYPTION_KEY_STORAGE[16] ZEPTO_PROG_CONSTANT_LOCATION = \
 { \
 	0x00, \
 	0x01, \
@@ -44,9 +59,12 @@ const uint8_t AES_ENCRYPTION_KEY[16] ZEPTO_PROG_CONSTANT_LOCATION = \
 	0x0e, \
 	0x0f, \
 }; \
+const uint8_t* AES_ENCRYPTION_KEY = AES_ENCRYPTION_KEY_STORAGE;
 
 #define DECLARE_DEVICE_ID \
 uint16_t DEVICE_SELF_ID = 1; /* definitely, not 0 */
+
+#endif // SA_DYN_PAIRING_ENABLED
 
 
 #endif // __ZEPTO_CONFIG_H__
